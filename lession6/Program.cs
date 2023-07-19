58ff3a996926cae8c28b8c4084980fcb0d6f7868
ibrohim795 <iismatov59@gmail.com>
2023-07-19 20:41:16 +0500

    2

--- lession6/Program.cs
+++ lession6/Program.cs
@@ -4,39 +4,8 @@ class Program
 {
     static void Main()
     {
-        string kirish = Console.ReadLine();
-        string natija = TopTakrorlanadiganBelgi(kirish);
-        Console.WriteLine(natija);
-    }
+        
 
-    static string TopTakrorlanadiganBelgi(string kirish)
-    {
-        char previousChar = '\0';
-        int count = 0;
-        string result = "";
-
-        foreach (char character in kirish)
-        {
-            if (character == previousChar)
-            {
-                count++;
-            }
-            else
-            {
-                if (count > 0)
-                {
-                    result += $"'{previousChar}' {count} marta kiritildi, ";
-                }
-                previousChar = character;
-                count = 1;
-            }
-        }
-
-        if (count > 0)
-        {
-            result += $"'{previousChar}' {count} marta kiritildi";
-        }
-
-        return result;
+        
     }
 }
